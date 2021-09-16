edc6a88047210c1aeb090222e4084a66d3787646
Ravi A <89862619+ravia-10@users.noreply.github.com>
2021-09-16 13:55:46 +0530

    No parameter and No return Type

---
+++ Clothes.java
@@ -0,0 +1,19 @@
+package com.xworkz.methods;
+
+public class Clothes {
+
+	// this method has no parameter and no return type:
+
+	public static void typeOfClothes() {
+
+		System.out.println("men's wear");
+		System.out.println("women's wear");
+		System.out.println("kid's wear");
+
+	}
+
+	public static void main(String[] args) {
+		Clothes.typeOfClothes();
+	}
+
+}
---
+++ Laptops.java
@@ -0,0 +1,23 @@
+package com.xworkz.methods;
+
+public class Laptops {
+
+	// this method has no parameter and no return type:
+
+	public static void laptopNames() {
+		System.out.println("Top laptops are : ");
+		System.out.println("1) HP");
+		System.out.println("2) Dell");
+		System.out.println("3) Lenovo");
+		System.out.println("4) Mac book");
+		System.out.println("5) samsung");
+		 
+		
+
+	}
+				
+	public static void main(String[] args) {
+		Laptops.laptopNames();
+	}
+
+}
